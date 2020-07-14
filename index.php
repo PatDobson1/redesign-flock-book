0050bee7b2217109318f0fc9cb8400f78f0549a4
patdobson <p.dobson@reply.com>
2020-07-14 18:12:07 +0100

    Added basic query

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
     $pdo = new PDO("mysql:host=localhost;dbname=fb_redesign", 'fb_redesign', 'Bjb29v&6__7][hGuye788');
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
+    // $pdo = self::$conn -> prepare("SELECT * FROM sheep WHERE id = :id");
+    $pdo = self::$conn -> prepare("SELECT * FROM sheep");
+    // $pdo -> bindParam(':id', $id);
+    if( $pdo -> execute() ){
+        if($results = $pdo -> fetch(PDO::FETCH_NAMED)){
+            $page = $results['content'];
+            return $page;
+        }else{
+            return "Page not found";
+        }
+    }
 
     $pdo = null;
 // ---------------------------------------------------------------------
