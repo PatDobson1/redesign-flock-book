9c50bcf11ca3fdb71dd798dffe671a0a7a07b0a6
patdobson <p.dobson@reply.com>
2020-07-14 18:06:22 +0100

    changed index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <h1>Flock Book</h1>
+
+Bjb29v&6__7][hGuye788
+
+fb_redesign
+
+<?php
+
+// -- Set up database connection ---------------------------------------
+    $pdo = new PDO("mysql:host=localhost;dbname=fb_redesign", 'fb_redesign', 'Bjb29v&6__7][hGuye788');
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+    $pdo = null;
+// ---------------------------------------------------------------------
+
+?>
