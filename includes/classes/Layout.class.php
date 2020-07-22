18eeb4216bc556ce4b2fedcb0dff461e41a7acb8
patdobson <p.dobson@reply.com>
2020-07-22 18:02:04 +0100

    Changes Added .htaccess Added livestock page Modified menu css

--- includes/classes/Layout.class.php
+++ includes/classes/Layout.class.php
@@ -14,57 +14,59 @@
                             <body>
                             <div class='overall'>
                                 <header>
-                                    <h1>$site_data[site_name] flockbook</h1>
-                                    <menu>
-                                        <ul>
-                                            <li><a href='$site_data[site_root]'>Dashboard</a></li>
-                                            <li class='dropdown'><a href='#'>Menu 1</a>
-                                                <ul>
-                                                    <li><a href='#'>subMenu long 1</a></li>
-                                                    <li><a href='#'>subMenu 2</a></li>
-                                                    <li><a href='#'>subMenu even longer 3</a></li>
-                                                    <li><a href='#'>subMenu 4</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class='dropdown'><a href='#'>Menu 2</a>
-                                                <ul>
-                                                    <li><a href='#'>subMenu 1</a></li>
-                                                    <li><a href='#'>subMenu 2</a></li>
-                                                    <li><a href='#'>subMenu 3</a></li>
-                                                    <li><a href='#'>subMenu 4</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href='#'>Menu 3</a></li>
-                                            <li><a href='#'>Menu 4</a></li>
-                                            <li><a href='#'>Menu 5</a></li>
-                                            <li class='dropdown'><a href='#'>Menu 6</a>
-                                                <ul>
-                                                    <li><a href='#'>subMenu 1</a></li>
-                                                    <li><a href='#'>subMenu 2</a></li>
-                                                    <li><a href='#'>subMenu 3</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </menu>
-                                    <rightMenu>
-                                        <ul>
-                                            <li class='dropdown'><a href='#'>Menu 1</a>
-                                                <ul>
-                                                    <li><a href='#'>subMenu 1</a></li>
-                                                    <li><a href='#'>subMenu a bit long 2</a></li>
-                                                    <li><a href='#'>subMenu 3</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href='#'>Menu 2</a></li>
-                                            <li class='dropdown'><a href='#'>Menu 3</a>
-                                                <ul>
-                                                    <li><a href='#'>subMenu 1</a></li>
-                                                    <li><a href='#'>subMenu 2</a></li>
-                                                    <li><a href='#'>subMenu 3</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </rightMenu>
+                                    <inner>
+                                        <h1>$site_data[site_name] flockbook</h1>
+                                        <menu>
+                                            <ul>
+                                                <li><a href='$site_data[site_root]'>Dashboard</a></li>
+                                                <li class='dropdown'><a>Menu 1</a>
+                                                    <ul>
+                                                        <li><a href='$site_data[site_root]/livestock'>Livestock</a></li>
+                                                        <li><a href='#'>subMenu 2</a></li>
+                                                        <li><a href='#'>subMenu even longer 3</a></li>
+                                                        <li><a href='#'>subMenu 4</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class='dropdown'><a href='#'>Menu 2</a>
+                                                    <ul>
+                                                        <li><a href='#'>subMenu 1</a></li>
+                                                        <li><a href='#'>subMenu 2</a></li>
+                                                        <li><a href='#'>subMenu 3</a></li>
+                                                        <li><a href='#'>subMenu 4</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href='#'>Menu 3</a></li>
+                                                <li><a href='#'>Menu 4</a></li>
+                                                <li><a href='#'>Menu 5</a></li>
+                                                <li class='dropdown'><a href='#'>Menu 6</a>
+                                                    <ul>
+                                                        <li><a href='#'>subMenu 1</a></li>
+                                                        <li><a href='#'>subMenu 2</a></li>
+                                                        <li><a href='#'>subMenu 3</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </menu>
+                                        <rightMenu>
+                                            <ul>
+                                                <li class='dropdown'><a href='#'>Menu 1</a>
+                                                    <ul>
+                                                        <li><a href='#'>subMenu 1</a></li>
+                                                        <li><a href='#'>subMenu a bit long 2</a></li>
+                                                        <li><a href='#'>subMenu 3</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href='#'>Menu 2</a></li>
+                                                <li class='dropdown'><a href='#'>Menu 3</a>
+                                                    <ul>
+                                                        <li><a href='#'>subMenu 1</a></li>
+                                                        <li><a href='#'>subMenu 2</a></li>
+                                                        <li><a href='#'>subMenu 3</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </rightMenu>
+                                    </inner>
                                 </header>
                                 <content>";
 
@@ -77,7 +79,10 @@
                 echo "
                         </content>
                         <footer>
-                            <p>F O O T E R</p>
+                            <div class='col_2'>
+                                <div>left</div>
+                                <div>right</div>
+                            </div>
                         </footer>
                     </div>
                 </body>
