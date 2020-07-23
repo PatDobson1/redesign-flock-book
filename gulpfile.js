2881ce68e2aac9b952d2c065c1b002d50d7763cf
patdobson <p.dobson@reply.com>
2020-07-23 18:04:52 +0100

    Updated build in compress css and create sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compileScss', function(){
   return gulp.src('includes/style/scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('includes/style'))
 });
-
-// gulp.task('watch', function(){
-//   gulp.watch('includes/style/scss/**/*.scss', ['sass']);
-// })
 
 gulp.task('default',function(){
     return gulp.watch('includes/style/scss/*.scss', gulp.series('compileScss'));
